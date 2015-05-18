0c5fdb5067b0d832d2f2c3572b509b509521ea6d
yumetodo <yume-wikijp@live.jp>
2015-05-18 16:33:54 +0900

    サイエンスショーページをとりあえず完成、新歓ページをそこはかとなく更新

---
+++ event/2015/event_june/resource/simple_slideshow.js
@@ -0,0 +1,12 @@
+﻿$(function(){
+	var setImg = '#slide';
+	var fadeSpeed = 1200;
+	var switchDelay = 4000;
+
+	$(setImg).children('img').css({opacity:'0'});
+	$(setImg + ' img:first').stop().animate({opacity:'1',zIndex:'20'},fadeSpeed);
+
+	setInterval(function(){
+		$(setImg + ' :first-child').animate({opacity:'0'},fadeSpeed).next('img').animate({opacity:'1'},fadeSpeed).end().appendTo(setImg);
+	},switchDelay);
+});//http://sedoriplan.com/jquery-fadein-fadeout-image/
