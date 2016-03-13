a7b0e2e4a13062be2dbe05f69e8c542109f7013a
yumetodo <yume-wikijp@live.jp>
2016-03-13 11:46:59 +0900

    fix #7

--- common/script/load.js
+++ common/script/load.js
@@ -6,12 +6,13 @@
  *
  * @return none.
  */
-var load_html_and_insert = function (html_url, insert_info_arr){
+var load_html_and_insert = function (html_url, insert_info_arr, lelative_path_to_root){
     $.ajax(html_url, {
         timeout : 1500,
         datatype: 'html'
     }).then(function(data){
-        var out_html = $($.parseHTML(data));//parse1
+        var data_t = data.replace(/\.\//g, lelative_path_to_root);
+        var out_html = $($.parseHTML(data_t));//parse1
         var i;
         for(i = 0; i < insert_info_arr.length; ++i){
             var size = insert_info_arr[i].length;
@@ -24,9 +25,15 @@ var load_html_and_insert = function (html_url, insert_info_arr){
         if(textStatus!=="success") {
             var txt = "<p>textStatus:"+ textStatus + "</p>" +
                 "<p>status:"+ jqXHR.status + "</p>" +
-                "<p>responseText : </p><pre>" + jqXHR.responseText.replace(/</, "&lt;") +"</pre>";
-            $('#page').html(txt);
-            $('#page2').html(txt);
+                "<p>responseText : </p><pre>" + jqXHR.responseText.replace(/</g, "&lt;") +"</pre>";
+            var i;
+            for(i = 0; i < insert_info_arr.length; ++i){
+                var size = insert_info_arr[i].length;
+                if(size < 2 || 3 < size) continue;
+                var $jqObj = $("#" + insert_info_arr[i][1]);
+                if(size === 2 || insert_info_arr[i][2]) $jqObj.empty();
+                $jqObj.append(txt);//insert
+            }
         }
     });
 };
@@ -34,7 +41,11 @@ var to_date_string = function(date){
     return date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate() + ' ' + date.getHours() + "時" + date.getMinutes() + "分";
 };
 var load_parts = function(lelative_path_to_root, insert_info_arr){
-    load_html_and_insert(lelative_path_to_root + 'common/html/parts.html', (insert_info_arr === undefined) ? [["pageBodySub", "pageBodySub"]] : insert_info_arr);
+    load_html_and_insert(
+        lelative_path_to_root + 'common/html/parts.html', 
+        (insert_info_arr === undefined) ? [["pageBodySub", "pageBodySub"]] : insert_info_arr, 
+        lelative_path_to_root
+    );
     var last_update_Date = new Date(document.lastModified);
     $('#copyright_year').empty().append(last_update_Date.getFullYear());
     $('#last_modified').empty().append("更新日:" + to_date_string(last_update_Date));
