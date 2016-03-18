9df2fd8d91a08a7419622f2e7d125f25f6b1a517
yumetodo <yume-wikijp@live.jp>
2016-03-18 10:03:16 +0900

    add js

---
+++ event/2016/event_march/resource/pseudo_screen_transition_svg.js
@@ -0,0 +1,13 @@
+var constexpr = {
+	'svg_cvt' : {
+		"chikaken" : ["id305"],
+		"tenmonken" : ["id266"],
+		"seibutuken" : ["id306", "id307"],
+		"bunngukenn" : [],
+		"chibilab" : ["id265"],
+		"ACM" : [],
+		"nikaken" : ["id304"],
+		"ichikaken" : ["id303"],
+		"rikoukaken" : []
+	}
+};
