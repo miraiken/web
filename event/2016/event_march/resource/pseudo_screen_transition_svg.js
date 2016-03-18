9a4340fce7b0754357b8530d25ba4873ce9104bf
yumetodo <yume-wikijp@live.jp>
2016-03-19 08:25:31 +0900

    fix syntax

--- event/2016/event_march/resource/pseudo_screen_transition_svg.js
+++ event/2016/event_march/resource/pseudo_screen_transition_svg.js
@@ -47,7 +47,7 @@ svg_manager = function(){
 	this.current_floor = "first";
 };
 svg_manager.prototype.show = function(floor, id){
-	var line_i = undefined;var box_i = undefined;
+	var line_i;var box_i;
 	var nodes = this.svg_detail[floor][id].childNodes;
 	for(var i = 0; i < nodes.length; ++i){
 		if(nodes[i].getAttribute("fill")!= "none" && nodes[i].getAttribute("stroke") == "none") box_i = i;
@@ -62,7 +62,7 @@ svg_manager.prototype.hide = function(floor, id){
 			nodes[i].setAttribute("fill", "rgb(255,255,255)");
 		}
 	}
-}
+};
 svg_manager.prototype.hide_all = function(){
 	for(var floor in svgid_name_cvt){
 		for(var id in svgid_name_cvt[floor]){
@@ -73,7 +73,7 @@ svg_manager.prototype.hide_all = function(){
 svg_manager.prototype.on_click = function(floor, id){
 	this.hide_all();
 	this.show(floor, id);
-}
+};
 /**
  * @brief register on click event
  * 
@@ -86,11 +86,12 @@ svg_manager.prototype.register_on_click_event = function(func){
 				return function(e){
 					that.on_click(floor, id);
 					if(undefined !== func) func(floor, id);
-				}
+				};
 			}(this, floor, id, func), false);//bind this
 		}
 	}
-}
+};
+console.log(document.getElementsByTagName("body")[0]);
 document.getElementsByTagName("body")[0].addEventListener("load", function(){
 	setInterval(function(){
 		var svg = new svg_manager();
