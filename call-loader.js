46dfdde7694f232115b1d57bea7c0505cca13194
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2017-08-15 09:32:21 +0900

    Add a workaround for an issue of module identification

--- call-loader.js
+++ call-loader.js
@@ -1,3 +1,10 @@
 module.exports = function(source) {
-  return source + ";module.exports = module.exports();";
+  /* module.id reference is a workaround for the issue addressed in the
+     following change in extract-text-webpack-plugin:
+     Refer to the entry point instead of the first module for default identifier
+     by akihikodaki · Pull Request #601 ·
+     webpack-contrib/extract-text-webpack-plugin
+     https://github.com/webpack-contrib/extract-text-webpack-plugin/pull/601
+  */
+  return source + ";module.exports = [[module.id, module.exports()]];";
 }
