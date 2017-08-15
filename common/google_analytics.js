9215645ecc212a98439a6e94c10f1833d3a4d843
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2017-08-16 00:14:00 +0900

    Fix do not track detection

--- common/google_analytics.js
+++ common/google_analytics.js
@@ -1,4 +1,4 @@
-if (!navigator.doNotTrack) {
+if (navigator.doNotTrack != 1) {
   window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 
   ga('create', 'UA-61470721-1', 'auto');
