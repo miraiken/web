594ae3bc471138107bf07ea5c60525c0574ede66
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2017-08-16 14:24:51 +0900

    Introduce PostCSS plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,8 @@
-module.exports = {};
+module.exports = {
+  plugins: [
+    require("stylelint"),
+    require("postcss-cssnext"),
+    require("postcss-reporter")(
+      {throwError: Boolean(process.env.POSTCSS_THROW_ERROR)}),
+  ],
+};
