69be11251e6ca49af8e6af38092d9b4cfc6de0f8
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2017-08-27 22:59:16 +0900

    Enforce flexible width on /web/

--- slideshow/index.js
+++ slideshow/index.js
@@ -1,21 +1,43 @@
 require('./index.css');
 
 document.addEventListener('DOMContentLoaded', function() {
-  var slideshow = document.getElementById('slideshow');
-  var previousUnbound = slideshow.lastChild;
+  var actives = document.getElementsByClassName('slideshow-active');
+  var tallests = Array.prototype.map.call(actives, function(active) {
+    var parent = active.parentNode;
+    var previousUnbound = parent.lastChild;
+    var tallest;
+    var tallestHeight = 0;
 
-  Array.prototype.forEach.call(slideshow.children, function(current, index) {
-    var previous = previousUnbound;
-    var next = slideshow.children[(index + 1) % slideshow.children.length];
+    Array.prototype.forEach.call(parent.children, function(current, index) {
+      var previous = previousUnbound;
+      var next = parent.children[(index + 1) % parent.children.length];
+      var height = current.getBoundingClientRect().height;
 
-    current.onanimationend = function() {
-      previous.className = '';
-      current.className = 'active';
-      next.className = 'next active';
-    };
+      if (tallestHeight < height) {
+        tallest = current;
+        tallestHeight = height;
+      }
 
-    previousUnbound = current;
+      current.onanimationend = function() {
+        previous.className = 'slideshow-inactive';
+        current.className = 'slideshow-active';
+        next.className = 'slideshow-next';
+      };
+
+      previousUnbound = current;
+    });
+
+    active.onanimationend();
+
+    return tallest;
   });
 
-  slideshow.firstChild.onanimationend();
+  onresize = requestAnimationFrame.bind(undefined, function() {
+    tallests.forEach(function(tallest) {
+      var height = tallest.getBoundingClientRect().height;
+      tallest.parentNode.style.height = height + 'px';
+    });
+  });
+
+  onresize();
 });
